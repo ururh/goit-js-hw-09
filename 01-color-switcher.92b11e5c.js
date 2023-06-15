9c9a150fb2fabe4a7d56b53f5140bb0def53696d
ururh <58848421+ururh@users.noreply.github.com>
2023-06-15 13:26:49 +0000

    Deploying to gh-pages from @ ururh/goit-js-hw-09@ba3128b3ac9de41ba84d96002d96f51f0b0afc7e 🚀

---
+++ 01-color-switcher.92b11e5c.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e;t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStop.disabled=!0,t.btnStart.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.92b11e5c.js.map
